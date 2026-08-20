10c6abb4fb62e8e7908b064a8c189c7f5cc38316
0xn4de <109149873+0xn4de@users.noreply.github.com>
2026-08-20 13:18:52 +0300

    add challenges

---
+++ challenges/02-falling-dutchman/Solution.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import "../../src/Interfaces.sol";
+import "./Interfaces.sol";
+import "./Constants.sol";
+
+contract FallingDutchman is Test {
+    address user = vm.envAddress("USER_ADDRESS");
+
+    function setUp() public {
+        vm.createSelectFork(vm.envString("ETH_RPC_URL"), FORK_BLOCK);
+        vm.deal(user, 0.1 ether);
+    }
+
+    function test_Solution() public {
+        vm.startBroadcast(user);
+        // Your solution goes here.
+        vm.stopBroadcast();
+        checkSolve();
+    }
+
+    function checkSolve() public view {
+        require(user.balance >= 4 ether, "not enough ETH");
+        console.log("Solved. Ending balance in ETH: %18e", user.balance);
+    }
+}
